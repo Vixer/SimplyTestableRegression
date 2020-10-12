9d86d434d6766de033be3b57c623b499bcca90e3
passi <passionfruit49@hotmail.com>
2020-10-12 13:12:58 +0100

    Adding DelayedElementFinder method for multiple elements

--- src/test/java/utils/DelayedElementFinder.java
+++ src/test/java/utils/DelayedElementFinder.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class DelayedElementFinder {
     public static WebElement findElement(WebDriver driver, By locator) {
         WebDriverWait wait = new WebDriverWait(driver, 5);
@@ -20,9 +22,30 @@ public class DelayedElementFinder {
             throw (e);
         }
         catch (Exception e){
-            System.out.println("TIMEOUT"+e.toString());
+            System.out.println(e.toString());
             throw (e);
         }
         return element;
+    }
+
+    public static List<WebElement> findElements(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        List<WebElement> elements;
+
+        try {
+            elements = driver.findElements(locator);
+        } catch (NoSuchElementException e) {
+            wait.until(ExpectedConditions.jsReturnsValue("return document.readyState==\"complete\";"));
+            elements = driver.findElements(locator);
+        }
+        catch (TimeoutException e){
+            System.out.println("TIMEOUT"+e.toString());
+            throw (e);
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+            throw (e);
+        }
+        return elements;
     }
 }
