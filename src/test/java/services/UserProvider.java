72394007f25d0a436088ce642614d85f2bf4611f
passi <passionfruit49@hotmail.com>
2020-10-12 18:29:08 +0100

    Adding UserProvider

---
+++ src/test/java/services/UserProvider.java
@@ -0,0 +1,18 @@
+package services;
+
+import models.User;
+
+import java.util.HashMap;
+
+public class UserProvider {
+    public HashMap users;
+
+    public UserProvider(){
+        this.users=new HashMap<String, User>();
+        this.users.put("RegisteredUser",new User("passionfruit49@hotmail.com","TestPass"));
+        this.users.put("RegisteredUserBadPassword", new User("passionfruit49@hotmail.com","Smoo"));
+        this.users.put("UnregisteredUser", new User("smoo@smoo.com","smoo"));
+        this.users.put("validUser3",new User("user3","pass"));
+
+    }
+}
